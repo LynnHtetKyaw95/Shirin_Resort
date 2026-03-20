e83aff919e6ac6f7dcb7c5dcb0e559f6edb9ba95
lhk <dklin1995@gmail.com>
2026-03-20 18:11:25 +0700

    authorization

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,14 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router";
 import { login as loginAPI } from "../../services/apiAuth";
 import toast from "react-hot-toast";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const { mutate: login, isPending } = useMutation({
     mutationFn: ({ email, password }) => loginAPI({ email, password }),
-    onSuccess: (user) => {
+    onSuccess: () => {
+      queryClient.invalidateQueries({ active: true });
       navigate("/dashboard");
     },
     onError: (error) => {
---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProtectedRoute = ({ children }) => {
+  const navigate = useNavigate();
+
+  // 1. Load the authenticated user
+  const { isPending, isAuthenticated } = useUser();
+
+  // 3. If there is No authenticated user, redirect to the login page
+  useEffect(() => {
+    if (!isAuthenticated && !isPending) {
+      navigate("/login");
+    }
+  }, [isPending, isAuthenticated, navigate]);
+
+  // 2. While loading, show Spinner
+  if (isPending) {
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+  }
+
+  // 4. If there is a user, render the app
+  if (isAuthenticated) {
+    return children;
+  }
+};
+
+export default ProtectedRoute;
