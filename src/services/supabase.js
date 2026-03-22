755620ae6f76f244afc04c048c243c3509316bcd
lhk <dklin1995@gmail.com>
2026-03-22 11:20:54 +0700

    some bug fixed for vercel

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://pxdwbyynxirzalbnqbgy.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4ZHdieXlueGlyemFsYm5xYmd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNjE3ODYsImV4cCI6MjA4ODgzNzc4Nn0.7L4Pmq_85yBeI3czuTJvNRPhbnTb4tMS8OIKC9RBbFs";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
+export { supabaseUrl };
